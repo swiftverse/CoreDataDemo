75adcd1dfb023daa0dbcb3c3035ca9a36be94c7c
swiftverse <bishupal2015@gmail.com>
2021-03-07 16:14:17 +0530

    first commit with push

---
+++ CoreDataDemo/Managers/CoreDataManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataManager.swift
+//  CoreDataDemo
+//
+//  Created by Muflis on 07/03/21.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    //MARK: - Properties
+    private let modelName: String
+    
+    //MARK:- initialization
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+}
