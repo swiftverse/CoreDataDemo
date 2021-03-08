06c005cc7748ef96d672f34f8cf7445c74575f9f
swiftverse <bishupal2015@gmail.com>
2021-03-08 10:52:46 +0530

    Create CoreDataManager.swift with DOM, MOC, PSC

--- CoreDataDemo/Managers/CoreDataManager.swift
+++ CoreDataDemo/Managers/CoreDataManager.swift
@@ -16,4 +16,55 @@ final class CoreDataManager {
     init(modelName: String) {
         self.modelName = modelName
     }
+    
+    //MARK:- managedobject context
+    private(set) lazy var managedObjectContext:  NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        return managedObjectContext
+        
+        
+    }()
+    
+    
+    //MARK:- NSMAnagedObjectModel
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let objectURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd")
+        else {
+            fatalError("Unable to find dataModel")
+        }
+        
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: objectURL)
+        else {
+            fatalError("Unable to load data model")
+        }
+        
+        return managedObjectModel
+    }()
+    
+    //MARK:- NSPersistentStorecoordinator
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        //instantiate PSC
+        let persistantStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        //create address for persistance store in Documents
+        
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        let directoryUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let persistentStoreUrl = directoryUrl.appendingPathComponent(storeName)
+        
+        
+        do {
+            let options = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
+         try   persistantStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreUrl, options: options)
+        }
+        
+        catch {
+            fatalError("No conection to sql lite")
+        }
+        
+        return persistantStoreCoordinator
+    }()
+    
+    
 }
